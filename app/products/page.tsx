d48386bc7369f9edb3aff1cd61b7b0c3d2f52d21
zubairxshah <zubairshahmasoomi@gmail.com>
2024-10-04 15:14:57 +0500

    Updated ver after debugging

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Product from "./[product]/page";
 import Link from "next/link";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
 export default async function Products() {
   const fetchData = await fetch("https://jsonplaceholder.typicode.com/posts");
   const res = await fetchData.json();
@@ -9,9 +16,9 @@ export default async function Products() {
     <div>
       <h3>Product List</h3>
       <ol>
-        {res.map((item: any, i: number) => {
+        {res.map((item: Post, i: number) => {
           return (
-            <li>
+            <li key={i}>
               <Link href={`/products/${item.id}`}>{item.title}</Link>
             </li>
           );
